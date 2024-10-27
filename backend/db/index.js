9df05d2a884303d66d08f5b9f7c6a915570bc101
Kajsa Asp <kajsalisaasp@gmail.com>
2024-10-27 17:36:06 +0100

    remove postgres code structure

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,8 +1,8 @@
-import pg from 'pg'
-const { Pool } = pg
+// import pg from 'pg'
+// const { Pool } = pg
 
-const pool = new Pool()
+// const pool = new Pool()
 
-export const query = (text, params, callback) => {
-  return pool.query(text, params, callback)
-}
+// export const query = (text, params, callback) => {
+//   return pool.query(text, params, callback)
+// }
